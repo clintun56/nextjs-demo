09b65bf1fc864ec69cf9d28c1acd5c288866b56d
clintun56 <clintun56@gmail.com>
2025-03-25 14:36:01 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Add a header section */}
       <header className="row-start-1 w-full text-center">
-        <h1 className="text-xl font-bold">Welcome to Next.js</h1>
+        <h1 className="text-xl font-bold">Welcome to clintun</h1>
       </header>
       
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
